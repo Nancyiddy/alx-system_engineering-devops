4a2a6e64313fa85ddcfd8274fda163d0e064d78e
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2023-03-27 15:31:35 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
-# Client configuration file with Puppet
-file_line {  'passw_no_autentication':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-file_line {  'passw_direction':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-}
+# set up your client SSH configuration file so that you can connect to a server without typing a password.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  }
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  }
